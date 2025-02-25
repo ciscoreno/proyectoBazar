50b9fd5ee1f686004e7c47021c9684d94d8e5918
ciscoreno ciscobv12@gmail.com <>
2025-02-25 20:20:57 -0300

    Se agregaron 3 gets, getProductosSinStock, getProductosDeVenta y getResumenDeVentaEnFecha

--- src/main/java/com/proyecto/bazar/service/IVentaService.java
+++ src/main/java/com/proyecto/bazar/service/IVentaService.java
@@ -1,26 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.proyecto.bazar.service;
 
+import com.proyecto.bazar.dto.ResumenVentasDTO;
+import com.proyecto.bazar.model.Producto;
 import com.proyecto.bazar.model.Venta;
+import java.time.LocalDate;
 import java.util.List;
 
-/**
- *
- * @author PC
- */
 public interface IVentaService {
     
     public List<Venta> getVentas();
     
     public Venta getVenta(Long idVenta);
     
+    public List<Producto> getProductosDeVenta(Long idVenta);
+    
+    public ResumenVentasDTO getResumenVentasEnFecha(LocalDate fecha_venta);
+    
     public void saveVenta(Venta venta);
     
     public void deleteVenta(Long id);
     
     public void editVenta(Venta venta);
+
     
 }
