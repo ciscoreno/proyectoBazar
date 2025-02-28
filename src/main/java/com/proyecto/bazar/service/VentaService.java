c272c48744a9b50ef8156729eb8db57a7156018b
ciscoreno <ciscobv12@gmail.com>
2025-02-28 19:35:12 -0300

    Estructura inicial proyecto bazar

--- src/main/java/com/proyecto/bazar/service/VentaService.java
+++ src/main/java/com/proyecto/bazar/service/VentaService.java
@@ -1,8 +1,13 @@
 
 package com.proyecto.bazar.service;
 
+import com.proyecto.bazar.dto.MayorVentaDTO;
+import com.proyecto.bazar.dto.ResumenVentasDTO;
+import com.proyecto.bazar.model.Cliente;
+import com.proyecto.bazar.model.Producto;
 import com.proyecto.bazar.model.Venta;
 import com.proyecto.bazar.repository.IVentaRepository;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,7 +29,52 @@ public class VentaService implements IVentaService {
         Venta ven = ventaRepo.findById(idVenta).orElse(null);
         return ven;
     }
-
+    
+     // Obtiene los productos de una determinada venta
+    @Override
+    public List<Producto> getProductosDeVenta(Long id_venta){
+        Venta venta = ventaRepo.findById(id_venta).orElse(null);
+        return venta.getListaProductos();
+    }
+    
+    @Override
+    public ResumenVentasDTO getResumenVentasEnFecha(LocalDate fecha){
+        List<Venta> ventasDelDia = ventaRepo.findByFechaVenta(fecha);
+        
+        int cantVentas = ventasDelDia.size();
+        double sumaMontos = 0;
+        
+        for(Venta venta : ventasDelDia){
+            sumaMontos += venta.getTotal();     
+        }
+        
+        return new ResumenVentasDTO(cantVentas, sumaMontos);    
+    }
+    
+    public MayorVentaDTO getVentaMayor(){
+        List<Venta> ventas = ventaRepo.findAll();
+        
+        Venta mayorVenta = ventas.get(0);
+        for(Venta venta : ventas){
+            if(venta.getTotal() > mayorVenta.getTotal()){
+                mayorVenta = venta;
+            }
+        }
+        
+        Cliente cliente = mayorVenta.getCliente();
+        
+        MayorVentaDTO infoVenta = new MayorVentaDTO(
+            mayorVenta.getCodigo_venta() ,
+            mayorVenta.getTotal(),
+            mayorVenta.getListaProductos().size(),
+            cliente.getNombre(),
+            cliente.getApellido()
+        );
+        
+        return infoVenta;
+        
+    }
+    
     @Override
     public void saveVenta(Venta venta) {
         ventaRepo.save(venta);
